bd1df63b534be3970ae387501a2f81cf8561e416
Prajwol Shrestha <shresthaprajwol161@gmail.com>
2024-09-06 09:07:08 +0545

    update: json to array conversion

--- jsondata.php
+++ jsondata.php
@@ -2,4 +2,14 @@
    $data=["name"=>"Prajwol","love"=>"playing Guitar"];
    $jsonData=json_encode($data);
    echo $jsonData;
+
+   $jsoninfo='{"name":"Prajwol","love":"playing Guitar"}';
+   $arrayform=json_decode($jsoninfo,true);//! if true is not defined then we will get class based object rather than array
+   echo "<br>";
+   print_r($arrayform);
+   echo "<br>";
+   foreach($arrayform as $data=>$value){
+       echo $data . ": " . $value;
+       echo "<br>";
+   }
 ?>
