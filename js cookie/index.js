3be00b0c11be07af08d9ac9eea8ad0bab9d0ecd4
Prajwol Shrestha <shresthaprajwol161@gmail.com>
2024-09-06 15:40:06 +0545

    test: js cookie setup

---
+++ js cookie/index.js
@@ -0,0 +1,12 @@
+//! set cookie
+document.cookie="name=pehu; expires= Sat, 08 Sep 2024 00:00:00 UTC; path=/";
+
+//! read cookie
+// let readCookie=document.cookie;
+// console.log(readCookie);
+
+// //! update cookie
+// document.cookie = "username=John; expires=Thu, 18 Dec 2013 12:00:00 UTC; path=/";
+
+//!delete cookie
+document.cookie = "name=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";//? here past data is used
